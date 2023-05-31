2ddf810b2ee0497fbc2400b068b255c9e288a19e
vai-mohit <mohit.vaishnav_cs.ccv19@gla.ac.in>
2023-06-01 01:05:28 +0530

    Added addition in main function

--- src/main/java/io/gitremote/demo.java
+++ src/main/java/io/gitremote/demo.java
@@ -4,6 +4,8 @@ public class demo {
 
 	public static void main(String[] args) {
 		int a = 10 , b =20;
-		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a-b);
+		System.out.println(a*b);
 	}
 }
