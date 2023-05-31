6e75b127cf1fc90cb799153f2cb447fadde7e238
rajatpandey95 <rajat.pandey_cs19@gla.ac.in>
2023-06-01 01:15:30 +0530

    demo

--- src/main/java/io/gitremote/demo.java
+++ src/main/java/io/gitremote/demo.java
@@ -1,11 +1,11 @@
 package io.gitremote;
 
 public class demo {
-
 	public static void main(String[] args) {
 		int a = 10 , b =20;
 		System.out.println(a-b);
 		System.out.println(a-b);
 		System.out.println(a*b);
+		System.out.println("Hello");
 	}
 }
